e6d17755bbf58c4c75bd31b44fb75cc32784438f
Tale152 <alessandro.talmi@studio.unibo.it>
2022-10-18 18:16:22 +0200

    removed routes

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,9 @@
 import express, {Express} from 'express';
 
 import {setupServer} from './setup/setup';
-import bindRoutes from './routes/routes';
 
 const server: Express = express();
 
 setupServer(server);
-bindRoutes(server);
 
 export default server;
