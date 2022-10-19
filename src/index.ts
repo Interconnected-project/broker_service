9b7f1b5a4e403bbed463fbefc72a01257f42a1ab
Tale152 <alessandro.talmi@studio.unibo.it>
2022-10-19 13:22:29 +0200

    crated Invoking Endpoints server

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,34 @@
 import createServer from './common/server';
 import NodeConnectionsHubSingleton from './nodes/NodeConnectionsHubSingleton';
 import EnvVariablesSingleton from './common/setup/EnvVariablesSingleton';
+import InvokingEndpointsConnectionsHubSingleton from './invokingEndpoints/InvokingEndpointsConnectionsHubSingleton';
+
+const env = EnvVariablesSingleton.instance;
 
 createServer(
   'Nodes',
-  EnvVariablesSingleton.instance.nodesPort,
+  env.nodesPort,
   NodeConnectionsHubSingleton.hub,
   (socket) => {
     socket.on('star wars', function (from, msg) {
       console.log(from + ': ' + msg);
       socket.emit('star wars', 'Grievous', 'General Kenobi');
+    });
+  }
+);
+
+createServer(
+  'Invoking Endpoints',
+  env.invokingEndpointsPort,
+  InvokingEndpointsConnectionsHubSingleton.hub,
+  (socket) => {
+    socket.on('jojo', function (from, msg) {
+      console.log(from + ': ' + msg);
+      socket.emit(
+        'star wars',
+        'Jotaro Kujo',
+        "I can't beat the s* out of you without getting closer."
+      );
     });
   }
 );
