e83ec57bbb64d3ba1b2d155f14223ae29d5190ba
Tale152 <alessandro.talmi@studio.unibo.it>
2022-10-19 22:37:17 +0200

    fixed tests

--- test/setup/EnvVariablesSingleton.test.ts
+++ test/setup/EnvVariablesSingleton.test.ts
@@ -1,5 +1,5 @@
 import { TEST_NODES_PORT_VARIABLE } from '../utils/set_env_variables';
-import EnvVariablesSingleton from '../../src/setup/EnvVariablesSingleton';
+import EnvVariablesSingleton from '../../src/common/setup/EnvVariablesSingleton';
 
 test('It should return the correct environment variables', () => {
   const singleton = EnvVariablesSingleton.instance;
