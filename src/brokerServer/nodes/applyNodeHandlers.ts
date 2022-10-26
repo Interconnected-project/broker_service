d282f80143f5531cc009dac59c5a7405cc4738ad
Tale152 <alessandro.talmi@studio.unibo.it>
2022-10-26 09:31:47 +0200

    applied initiate connection handler to node

--- src/brokerServer/nodes/applyNodeHandlers.ts
+++ src/brokerServer/nodes/applyNodeHandlers.ts
@@ -1,8 +1,10 @@
 import Connection from '../../common/connectionsHub/Connection';
 import answerConnectionHandler from '../../handlers/answerConnection/answerConnectionHandler';
+import initializeConnectionHandler from '../../handlers/initializeConnection/initializeConnectionHandler';
 import recruitmentAcceptHandler from '../../handlers/recruitmentAccept/recruitmentAcceptHandler';
 
 export default function applyNodeHandlers(connection: Connection) {
   recruitmentAcceptHandler(connection);
   answerConnectionHandler(connection);
+  initializeConnectionHandler(connection);
 }
