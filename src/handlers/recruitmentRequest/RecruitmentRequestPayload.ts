9b983c1f3987109fd7d3628a1a6b5b88ff828e06
Tale152 <alessandro.talmi@studio.unibo.it>
2022-10-26 10:22:45 +0200

    moved controls from getter to constructor

--- src/handlers/recruitmentRequest/RecruitmentRequestPayload.ts
+++ src/handlers/recruitmentRequest/RecruitmentRequestPayload.ts
@@ -4,33 +4,48 @@ import checkAndReturnString from '../../common/util/checkAndReturnString';
 
 export default class RecruitmentRequestPayload {
   // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  private _invokingEndpointId: string;
+  private _initiatorId: string;
+  private _initiatorRole: string;
+  private _operationId: string;
+  private _nodesToReach: number;
+
   constructor(private payload: any) {
     if (payload === undefined || payload == null) {
       throw new Error();
     }
+    this._invokingEndpointId = checkAndReturnString(
+      this.payload.invokingEndpointId
+    );
+    this._initiatorId = checkAndReturnString(this.payload.initiatorId);
+    this._initiatorRole = checkAndReturnString(this.payload.initiatorRole);
+    if (
+      this._initiatorRole !== Roles.INVOKING_ENDPOINT &&
+      this._initiatorRole != Roles.NODE
+    ) {
+      throw new Error();
+    }
+    this._operationId = checkAndReturnString(this.payload.operationId);
+    this._nodesToReach = checkAndReturnInteger(this.payload.nodesToReach);
   }
 
   get invokingEndpointId(): string {
-    return checkAndReturnString(this.payload.invokingEndpointId);
+    return this._invokingEndpointId;
   }
 
   get initiatorId(): string {
-    return checkAndReturnString(this.payload.initiatorId);
+    return this._initiatorId;
   }
 
   get initiatorRole(): string {
-    const res = checkAndReturnString(this.payload.initiatorRole);
-    if (res !== Roles.INVOKING_ENDPOINT && res != Roles.NODE) {
-      throw new Error();
-    }
-    return res;
+    return this._initiatorRole;
   }
 
   get operationId(): string {
-    return checkAndReturnString(this.payload.operationId);
+    return this._operationId;
   }
 
   get nodesToReach(): number {
-    return checkAndReturnInteger(this.payload.nodesToReach);
+    return this._nodesToReach;
   }
 }
